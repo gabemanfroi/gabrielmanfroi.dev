49db70e499c2acbe57135323c877e8f10e709cb0
Gabriel Manfroi <gabriel.manfroi@edreamsodigeo.com>
2023-08-02 23:46:46 +0100

    :hammer: starts consuming api data

--- src/components/LeftSideBar/ProfilePicture.tsx
+++ src/components/LeftSideBar/ProfilePicture.tsx
@@ -1,16 +1,23 @@
 import Image from 'next/image';
 import { Grid } from '@mui/material';
 import React from 'react';
+import { useQueryUser } from '@/hooks/queries/useQueryUser';
 
 export const ProfilePicture = () => {
-  return <Grid item xs={5} maxHeight={'300px'}>
-    <Image
-      src={'https://wpriverthemes.com/HTML/drake/assets/images/me.jpg'}
-      alt={'something'}
-      width={0}
-      height={0}
-      sizes='100vw'
-      style={{ width: '100%', height: 'auto', borderRadius: '30px' }} // optional
-    />
-  </Grid>;
+  const { getUserByNameData, getUserByNameLoading } = useQueryUser();
+
+  if (getUserByNameLoading) return <></>;
+
+  return (
+    <Grid item xs={5} maxHeight={'300px'}>
+      <Image
+        src={`${process.env.NEXT_PUBLIC_STRAPI_HOST}${getUserByNameData.data.attributes.profilePicture.data.attributes.url}`}
+        alt={'something'}
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: '100%', height: 'auto', borderRadius: '30px' }} // optional
+      />
+    </Grid>
+  );
 };
