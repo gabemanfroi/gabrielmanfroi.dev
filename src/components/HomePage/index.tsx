7560dbf5c120f1e32da182de807ab8b621f67eca
Gabriel Manfroi <gabriel.manfroi@edreamsodigeo.com>
2023-08-02 23:46:28 +0100

    :recycle: minor refactoring

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,11 +1,8 @@
 import { Grid } from '@mui/material';
 import { LeftSideBar } from '@/components/LeftSideBar';
 import { MainContent } from '@/components/MainContent';
-import { useQueryUser } from '@/hooks/queries/useQueryUser';
 
 export const HomePage = () => {
-  const { getUserByNameData } = useQueryUser();
-  console.log(getUserByNameData);
   return (
     <Grid container px={3} py={9} gap={30} height={'100%'} overflow={'hidden'}>
       <Grid item container xs={2.8} height={'100%'}>
